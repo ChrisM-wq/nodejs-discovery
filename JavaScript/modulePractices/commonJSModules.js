fefd0f0602463a615d85b8c1c86f090d77a0b30b
Chris McHardy <chrismchardy1@gmail.com>
2024-11-07 11:33:06 +0200

    Add a test on using common JS modules

---
+++ JavaScript/modulePractices/commonJSModules.js
@@ -0,0 +1,8 @@
+// Export
+export.addToCart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart! (shipping cost is ${shippingCost})`);
+};
+
+// Import
+const { addToCart } = require("./commonJSModules.mjs")
