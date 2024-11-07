e1e4f0a06da384cc0e42ea9d5066dbff66b9b518
Chris McHardy <chrismchardy1@gmail.com>
2024-11-07 12:10:28 +0200

    Update packages, leaflet, lodash-es, parcel and comment out common JS modules to avoid error

--- JavaScript/modulePractices/commonJSModules.js
+++ JavaScript/modulePractices/commonJSModules.js
@@ -1,8 +1,8 @@
 // Export
-export.addToCart = function (product, quantity) {
-  cart.push({ product, quantity });
-  console.log(`${quantity} ${product} added to cart! (shipping cost is ${shippingCost})`);
-};
+// export.addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added to cart! (shipping cost is ${shippingCost})`);
+// };
 
 // Import
-const { addToCart } = require("./commonJSModules.mjs")
+// const { addToCart } = require("./commonJSModules.mjs")
